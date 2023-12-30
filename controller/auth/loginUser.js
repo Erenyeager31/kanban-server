7dcddff51e3cbe0dfee5019756df6ffca704b7ec
Dishant Shah <dishantshah3133@gmail.com>
2023-12-30 11:51:05 +0530

    sameSite --> secure

--- controller/auth/loginUser.js
+++ controller/auth/loginUser.js
@@ -63,7 +63,7 @@ const loginUser = async (req, res) => {
                     path: "/",
                     expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
                     httpOnly: true,
-                    sameSite: "None"
+                    sameSite: "None;Secure"
                 })
             .json({
                 success: true,
